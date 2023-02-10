b0e87ddf5fdb551f732ca1847f17713e70f9869d
okonkwo2134 <70862328+okonkwo2134@users.noreply.github.com>
2023-02-10 10:31:29 -0600

    changed wordle_text

--- wordle_text.py
+++ wordle_text.py
@@ -53,8 +53,7 @@ def game(secret_words, all_words):
             print()
             rounds += 1
         else:
-            print()
-            print(guess, 'is not a valid word. Please try again.')
+            print(guess, 'is not a valid word. Please try again.\n')
     results = ('Genius', 'Magnificent', 'Impressive', 'Splendid', 'Great', 'Phew')
     if rounds < 6:
         print(f'You win. {results[rounds]}!\n')
